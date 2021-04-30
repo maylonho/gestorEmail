d4a14828d84ec0a021be3d044cac3163cfbb0d78
Maylon <oliveira_maylon@hotmail.com>
2021-04-30 13:43:06 -0300

    a

--- php/Conexao.php
+++ php/Conexao.php
@@ -8,8 +8,8 @@ define('DB', 'gestor-email');
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Nao foi possivel conectar');
 */
 
-$conexao = mysqli_connect("localhost", "root", "");
-$bd = mysqli_select_db($conexao, "gestor-email") or die ("Erro BD");
+$conexao = mysqli_connect("localhost", "u874148780_root", "gestorEmail131995");
+$bd = mysqli_select_db($conexao, "u874148780_gestorEmail") or die ("Erro BD");
 
 mysqli_query($conexao, "SET NAMES utf8");
 date_default_timezone_set('America/Sao_Paulo');
